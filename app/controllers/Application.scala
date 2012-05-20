5dc64fedf1eb69a08013d4593af96a438bc0f8d7
Malte Lenz <malte.lenz@gmail.com>
2012-05-20 22:17:55 +0200

    Added PageStatistic model, and form and dummy page for creating instances.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,14 +2,31 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import models.PageStatistic
 
 object Application extends Controller {
+
+  val newForm = Form(
+    "label" -> nonEmptyText
+  )
   
   def index = Action {
-    Ok("Hello World!")
+    Ok(views.html.index(newForm))
   }
   
-  def getPage = TODO
+  def getPage = Action { implicit request =>
+    newForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(errors)),
+      label => {
+        PageStatistic.create(label)
+        //TODO: replace 1 with real value
+        Redirect(routes.Application.showResult(1))
+      }
+    )
+  }
 
   def showResult(id: Long) = TODO
 
---
+++ app/models/PageStatistic.scala
@@ -0,0 +1,7 @@
+package models
+
+case class PageStatistic(id: Long, label: String)
+
+object PageStatistic {
+  def create(label: String) {}
+}
