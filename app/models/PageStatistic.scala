1c016c8d489fc094285ff6f3043f06eae3f02ada
Malte Lenz <malte.lenz@gmail.com>
2012-05-28 20:11:41 +0200

    Retrieve given url and show length of HTML.

--- app/models/PageStatistic.scala
+++ app/models/PageStatistic.scala
@@ -1,7 +1,9 @@
 package models
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 
-case class PageStatistic(hash: String, pageURI: String)
-
-object PageStatistic {
-
+case class PageStatistic(hash: String, pageURI: String) {
+  val url = pageURI
+  var doc: Document = Jsoup.connect(url).get()
+  val strLen = doc.outerHtml().length()
 }
