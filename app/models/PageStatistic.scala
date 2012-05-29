5d9f3e17a4b829046790e7f0c5e4f5efb7d344f3
Malte Lenz <malte.lenz@gmail.com>
2012-05-29 22:41:28 +0200

    Handle both http, https, and no protocol.

--- app/models/PageStatistic.scala
+++ app/models/PageStatistic.scala
@@ -3,9 +3,19 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
 case class PageStatistic(hash: String, pageURI: String) {
-  val url = pageURI
+  val url = PageStatistic.handlePageURI(pageURI)
   var doc: Document = Jsoup.connect(url).get()
   val strLen = doc.outerHtml().length()
   val urlLen = url.length()
   val title = doc.title()
+}
+
+object PageStatistic {
+  def handlePageURI(url:String) = { 
+    val httppattern = "http(?:s)+://(.*)".r
+    url match {
+        case httppattern(x) => url
+        case y:String => ("http://" + y)
+    }
+  }
 }
