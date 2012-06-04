86f7cc868d46cfde699ab7819d63ad77ec364f74
Malte Lenz <malte.lenz@gmail.com>
2012-06-04 21:58:21 +0200

    Added nr of external links, and chart for character uses.

--- app/models/PageStatistic.scala
+++ app/models/PageStatistic.scala
@@ -5,6 +5,11 @@ import collection.JavaConversions._
 
 case class PageStatistic(hash: String, pageURI: String) {
   val url = PageStatistic.handlePageURI(pageURI)
+  val urlDomainPart = url.take(url.indexOf("/", url.indexOf("://") + 3) match {
+	    case -1 => url.length
+	    case s => s
+	  })
+  val domain = urlDomainPart.drop(urlDomainPart.indexOf("://") + 3)
   var doc: Document = Jsoup.connect(url).get()
   val strLen = doc.outerHtml().length()
   val urlLen = url.length()
@@ -13,6 +18,9 @@ case class PageStatistic(hash: String, pageURI: String) {
   val nrTags = doc.getAllElements().size();
   val tagHistogram = ((for(tag <- (doc.getAllElements().map(s => s.tagName()) groupBy identity))
 	  						yield (tag._1, tag._2.length)).toList sortWith (_._2 > _._2) take 10) reverse
+  val charHistogram = ((for(tag <- (doc.outerHtml() groupBy identity))
+	  						yield (tag._1, tag._2.length)).toList sortWith (_._2 > _._2) take 10) reverse
+  val nrAExternal = doc.getElementsByTag("a").filter (s => s.attr("href").contains("://") && !s.attr("href").startsWith(urlDomainPart)) length
 }
 
 object PageStatistic {
